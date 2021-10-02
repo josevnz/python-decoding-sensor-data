eda074b73c84e621a1686f3e5f360afc2772689e
Jose Vicente Nunez <josevnz@kodegeek.com>
2021-10-02 05:55:54 -0400

    Commit for PluralSight Project

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -5,6 +5,7 @@ from temperature_info import TemperatureData
 from humidity_info import HumidityData
 from statistics import mean
 from particle_count_info import ParticleData
+from energy_info import EnergyData
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -47,7 +48,6 @@ print("House Humidity sensor records for date: {} = {}".format(
     test_date.strftime("%m/%d/%y"), len(recs)))
 print("\tAverrage: {} humidity".format(mean(recs)))
 
-# Module 5 code here:
 # Module 4B
 particle_data = ParticleData(data)
 recs = particle_data.get_data_by_area(rec_area=test_area)
@@ -57,7 +57,6 @@ print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 
-# Module 6 code here:
 recs = particle_data.get_data_by_date(rec_date=test_date)
 print("\nHouse Particle sensor records for date: {} = {}".format(
     test_date.strftime("%m/%d/%y"), len(recs)))
@@ -66,3 +65,15 @@ print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 # Module 5 code here:
+energy_data = EnergyData(data)
+recs = energy_data.get_data_by_area(rec_area=test_area)
+print("\nHouse Energy sensor records for area {} = {}".format(test_area, len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
+
+# Module 6 code here:
+recs = energy_data.get_data_by_date(rec_date=test_date)
+print("House Energy sensor records for date: {} = {}".format(
+    test_date.strftime("%m/%d/%y"), len(recs)))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy))
